e22632f888c5ff9484e2b3867b2de70f295e0338
Vu Dat <vudat42@gmail.com>
2023-03-23 22:45:52 +0700

    cau dieu kien

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,22 +1,40 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+    state = {
+        isShowListUsers: true
+    }
+
+    toggleListUsers = () => {
+        this.setState({
+            isShowListUsers: !this.state.isShowListUsers
+        })
+    }
 
     render() {
         const { listUsers } = this.props;
         // console.log(this.props?.listUsers)
         return (
             <div>
-                {listUsers.map((user) => {
-                    const {id, name, age} = user;
-                    return (
-                        <div key={id}>
-                            {name}
-                            {age}
-                            <hr />
-                        </div>
-                    )
-                })}
+                <div>
+                    <span onClick={this.toggleListUsers}>
+                        {this.state.isShowListUsers ? "Hide list users" : "Show list users"}
+                    </span>
+                </div>
+                {this.state.isShowListUsers &&
+                    <div>
+                        {listUsers.map((user) => {
+                            const { id, name, age } = user;
+                            return (
+                                <div key={id} className={age <= 30 ? "red" : "blue"}>
+                                    {name}
+                                    {age}
+                                    <hr />
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         )
     }
