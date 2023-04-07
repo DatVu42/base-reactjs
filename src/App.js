1c40d265b5a960011d02e31b00afd69d2dda857a
Vu Dat <vudat42@gmail.com>
2023-04-07 16:23:40 +0700

    test link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 
 const App = () => {
 
   return (
     <div className="app-container">
       <Header />
+      <div>Test Links</div>
+      <button><Link to="/users">Go to User page</Link></button>
+      <button><Link to="/admin">Go to Admin page</Link></button>
     </div>
   );
 }
