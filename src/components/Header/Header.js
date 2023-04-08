6b5c095f38b3c2985392075a013a8e60f971ea12
Vu Dat <vudat42@gmail.com>
2023-04-08 09:16:30 +0700

    index routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,25 +2,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <NavLink className='navbar-brand' to='/'>React Basic</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link className='nav-link' to='/'>Home</Link>
-                        <Link className='nav-link' to='/users'>Users</Link>
-                        <Link className='nav-link' to='/admin'>Admin</Link>
+                        <NavLink className='nav-link' to='/'>Home</NavLink>
+                        <NavLink className='nav-link' to='/users'>Users</NavLink>
+                        <NavLink className='nav-link' to='/admin'>Admin</NavLink>
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                            <NavDropdown.Item>Login</NavDropdown.Item>
+                            <NavDropdown.Item>Logout</NavDropdown.Item>
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
