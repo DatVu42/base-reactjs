cf1fc42d32c458b624c44137e149236aad208a9a
Vu Dat <vudat42@gmail.com>
2023-04-12 13:59:50 +0700

    design homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,8 +1,12 @@
-
+import videoHomePage from '../../assets/video-homepage.mp4';
 
 const HomePage = (props) => {
     return (
-        <div className="homepage-container">HomePage component</div>
+        <div className="homepage-container">
+            <video autoPlay muted loop>
+                <source src={videoHomePage} type="video/webm" />
+            </video>
+        </div>
     )
 }
 
